c523607e075c0c0196b81891e9379b43861801dd
kotecoder <javajuniordev@yandex.ru>
2017-01-30 14:52:37 +0200

    HW_3 fix problem with spring configuration and project structure

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
@@ -24,7 +24,7 @@ import static ru.javawebinar.topjava.UserTestData.USER;
  * GKislin
  * 13.03.2015.
  */
-@ContextConfiguration(locations={"classpath:spring/spring-app.xml","classpath:spring/spring-db.xml"})
+@ContextConfiguration({"classpath:spring/spring-app.xml","classpath:spring/mock.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 
 public class InMemoryAdminRestControllerSpringTest {
