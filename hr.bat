8ed8e1989bdf7555d475be9628f58ec48db19294
kotecoder <javajuniordev@yandex.ru>
2017-03-11 19:45:40 +0200

    11 09 heroku

---
+++ hr.bat
@@ -0,0 +1,2 @@
+call mvn -B -s settings.xml -DskipTests=true clean package
+call java -.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.warDspring
